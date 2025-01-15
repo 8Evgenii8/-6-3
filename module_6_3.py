424627d2a5e764f24fb38167c4bc80e3dc0d43f2
8Evgenii8 <jiganpro77@gmail.com>
2025-01-15 14:44:45 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -11,12 +11,12 @@ class Animal:
 
 
     def move(self, dx, dy, dz):
-        if self._cords[2] + dz < 0:
+        self._cords[0] = dx * self.speed
+        self._cords[1] = dy * self.speed
+        if dz * self.speed < 0:
             print("It's too deep, i can't dive :(")
         else:
-            self._cords[0] += dx * self.speed
-            self._cords[1] += dy * self.speed
-            self._cords[2] += dz * self.speed
+            self._cords[2] = dz * self.speed
 
     def get_cords(self):
         print(f'X:{self._cords[0]} Y: {self._cords[1]} Z: {self._cords[2]}')
@@ -56,7 +56,7 @@ class PoisonousAnimal(Animal):
     def __init__(self, speed):
         super().__init__(speed)
 
-class Duckbill(Bird, PoisonousAnimal, AquaticAnimal):
+class Duckbill(PoisonousAnimal, Bird , AquaticAnimal):
     sound = "Click-click-click"
 
     def __init__(self, speed):
@@ -69,6 +69,9 @@ db.speak()
 db.attack()
 db.move(1, 2, 3)
 db.get_cords()
+db.dive_in(6)
+db.get_cords()
+db.lay_eggs()
 db.dive_in(6)
 db.get_cords()
 db.lay_eggs()
